894e919e5e30b45717dbb68a3f87283bee160247
Leon <leon_geertinger@hotmail.com>
2026-09-03 14:18:07 +0200

    Add getBoard function

--- ChessBoard-SUT26-Leon_Geertinger_Johansson/Program.cs
+++ ChessBoard-SUT26-Leon_Geertinger_Johansson/Program.cs
@@ -5,6 +5,24 @@ namespace ChessBoard_SUT26_Leon_Geertinger_Johansson
     {
         static void Main(string[] args)
         {
+            char[] getBoard(int size = 10)
+            {
+                char[] board = new char[size * size];
+                char[] square = ['■', '□'];
+
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (size % 2 == 0)
+                    {
+                        if (i != 0 && i % size == 0)
+                        {
+                            square = square.Reverse().ToArray();
+                        }
+                    }
+                    board[i] = square[i % 2];
+                }
+                return board;
+            }
         }
     }
 }

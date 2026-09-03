043a7321e8241a5b314c2b407ea70c924238ac46
leon_ <leon_@LEON>
2026-09-03 13:46:06 +0200

    Add project files.

---
+++ ChessBoard-SUT26-Leon_Geertinger_Johansson/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+namespace ChessBoard_SUT26_Leon_Geertinger_Johansson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

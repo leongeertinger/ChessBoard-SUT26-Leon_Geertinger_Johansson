32648bd7887efd7e433c4664a1f50c6905ffa925
Leon <leon_geertinger@hotmail.com>
2026-09-03 14:27:47 +0200

    Add printBoard function and comments

--- ChessBoard-SUT26-Leon_Geertinger_Johansson/Program.cs
+++ ChessBoard-SUT26-Leon_Geertinger_Johansson/Program.cs
@@ -14,14 +14,28 @@ namespace ChessBoard_SUT26_Leon_Geertinger_Johansson
                 {
                     if (size % 2 == 0)
                     {
+                        //If the size is uneven we reverse the order of squares every new row so it lines up correctly.
                         if (i != 0 && i % size == 0)
                         {
                             square = square.Reverse().ToArray();
                         }
                     }
+                    //Add squares to board alternating between white and black squares.
                     board[i] = square[i % 2];
                 }
                 return board;
+            }
+            void printBoard(char[] board, int size)
+            {
+                for (int i = 0; i < board.Length; i++)
+                {
+                    Console.Write(board[i]);
+                    //Creates a new row after printing enough squares to accomodate for the size of the board.
+                    if ((i + 1) % size == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
             }
         }
     }
